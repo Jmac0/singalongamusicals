393f51fb906df29173ead92d69a9939652203c82
JamieMac <Jamiejmackenzie@gmail.com>
2024-10-08 11:59:17 +0100

    feat(pages): basic folder setup for new pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,9 @@ export default function HomePage() {
           <Logo className='w-16' />
           <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
           <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
+            Easy now Rude boy A starter for Next.js, Tailwind CSS, and
+            TypeScript with Absolute Import, Seo, Link component, pre-configured
+            with Husky{' '}
           </p>
           <p className='mt-2 text-sm text-gray-700'>
             <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
---
+++ src/app/promo/request-a-song/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jimbob: React.FC = () => {
+  return (
+    <div>
+      <h1>Request a song</h1>
+    </div>
+  );
+};
+
+export default Jimbob;
